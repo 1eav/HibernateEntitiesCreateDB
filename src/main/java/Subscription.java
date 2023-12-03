518ffc2cbe6ac84ca6553962b2e23c8a6a69ac4b
1eav <6EAV.pop.corn@gmail.com>
2023-12-03 20:29:10 +0300

    Initial commit

---
+++ src/main/java/Subscription.java
@@ -0,0 +1,53 @@
+import composite.SubscriptionIdKey;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Subscriptions")
+public class Subscription {
+    @EmbeddedId
+    private SubscriptionIdKey id;
+    @Column(name = "student_id", insertable = false, updatable = false)
+    private int studentId;
+    @Column(name = "course_id", insertable = false, updatable = false)
+    private int courseId;
+
+    @Column(name = "subscription_date")
+    private Date subscriptionDate;
+
+    public SubscriptionIdKey getId() {
+        return id;
+    }
+
+    public void setId(SubscriptionIdKey id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public Date getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public void setSubscriptionDate(Date subscriptionDate) {
+        this.subscriptionDate = subscriptionDate;
+    }
+}
